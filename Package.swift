b68bdbcc8511120b1b164379030c9b8f9c96fe67
Courtland Bueno <courtcrit@gmail.com>
2019-05-29 01:11:17 -0700

    separated copied `VerticalTree` code into module

--- Package.swift
+++ Package.swift
@@ -14,13 +14,14 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         .package(url: "https://github.com/CourtlandBueno/SwiftSoup.git", .branch("master")),
+        .package(url: "https://github.com/CourtlandBueno/VerticalTree.git", .branch("master")),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "SoupKit",
-            dependencies: ["SwiftSoup"]),
+            dependencies: ["SwiftSoup", "VerticalTree"]),
         .testTarget(
             name: "SoupKitTests",
             dependencies: ["SoupKit"]),
--- Sources/SoupKit/VerticalTree/ElementWrapper.swift
+++ Sources/SoupKit/VerticalTree/ElementWrapper.swift
@@ -7,6 +7,7 @@
 
 import SwiftSoup
 import Foundation
+import VerticalTree
 
 public final class ElementWrapper: VerticalTreeNode, Infomation {
     
