d960845417162cbe4097ef2d28988484475d3c72
Courtland Bueno <courtcrit@gmail.com>
2019-05-29 02:02:35 -0700

    removed SPM generated initial file

--- Sources/SoupKit/SoupKit.swift
+++
@@ -1,5 +0,0 @@
-import SwiftSoup
-
-struct SoupKit {
-    var text = "Hello, World!"
-}
--- Tests/SoupKitTests/SoupKitTests.swift
+++ Tests/SoupKitTests/SoupKitTests.swift
@@ -6,7 +6,8 @@ final class SoupKitTests: XCTestCase {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct
         // results.
-        XCTAssertEqual(SoupKit().text, "Hello, World!")
+        let text = "Hello, World!"
+        XCTAssertEqual(text, "Hello, World!")
     }
 
     static var allTests = [
