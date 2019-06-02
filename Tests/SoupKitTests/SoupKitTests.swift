9656f22b73277d7051eafd6a94e367ff490935a6
Courtland Bueno <courtcrit@gmail.com>
2019-06-02 07:48:02 -0700

    fixing bundle urls

--- Tests/SoupKitTests/SoupKitTests.swift
+++ Tests/SoupKitTests/SoupKitTests.swift
@@ -8,6 +8,19 @@ final class SoupKitTests: XCTestCase {
         // results.
         let text = "Hello, World!"
         XCTAssertEqual(text, "Hello, World!")
+        
+    }
+    
+    func testCachedKnownAttributesURL() {
+        let bundle = Bundle(for: InternalClass.self)
+//        let url = URL(fileReferenceLiteralResourceName: "known_attributes.plist")
+        
+        let url = bundle.url(forResource: "known_attributes", withExtension: "plist")
+        XCTAssertNotNil(url)
+//        let data = try! Data.init(contentsOf: url)
+//        let cache = try! KnownAttribute.load(from: data)
+//        
+//        XCTAssert(!cache.isEmpty)
     }
 
     static var allTests = [
